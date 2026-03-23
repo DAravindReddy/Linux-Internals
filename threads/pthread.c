392a64a9dc07faf2355573808909f2e3bb6c0b67
DAravindReddy <aravindreddy0726@gmail.com>
2026-03-23 12:58:52 +0530

    adding thread files

---
+++ threads/pthread.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+
+void *thread_function(void *arg)
+{
+	int i = 5;
+	while(i--)
+	{
+		printf("Thread is executiong and tid: %ld\n", pthread_self());
+		sleep(1);
+	}
+	return NULL;
+}
+
+int main()
+{
+	pthread_t tid;
+	if(pthread_create(&tid, NULL, thread_function, NULL) != 0)
+	{
+		printf("Thread is not created\n");
+		return -1;
+	}
+	pthread_join(tid, NULL);
+
+}
